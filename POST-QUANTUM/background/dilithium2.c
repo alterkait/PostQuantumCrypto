95c0db1ca328f42e0e04c245202dac334c1c4b86
libpqcrypto <libpqcrypto@DESKTOP-PEAPL9I.localdomain>
2023-08-21 13:19:16 +0100

    Replicating dilithium include

---
+++ POST-QUANTUM/background/dilithium2.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "../randombytes.h"
+#include "../sign.h"
+#include <stdlib.h>
+
+int main(){
+	printf("hellow");
+	return 0;
+}
+
